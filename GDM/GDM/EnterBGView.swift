efdfce344981194775a5dee83ab9c2089f999f9e
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-11-13 09:57:01 -0500

    11/13/19 10:00am First submission

--- GDM/GDM/EnterBGView.swift
+++ GDM/GDM/EnterBGView.swift
@@ -9,18 +9,40 @@ import SwiftUI
 
 struct EnterBGView: View {
     
-    @State var BGText : String
     @State var selectedDate = Date()
+
+    var dateClosedRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+        let max = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        return min...max
+    }
+    
+    @State var bgText : String
+    var bgInt : Int
  
     var body: some View {
-        
-        TextField("Enter BG", text: $BGText)
-        .textFieldStyle(RoundedBorderTextFieldStyle())
-        .keyboardType(.numberPad)
-
-        
-        //DatePicker($selectedDate, displayedComponents: .date)
+        VStack{
+            Text("Enter BG")
+            TextField("mg/dL", text: $bgText)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .keyboardType(.numberPad)
+            .onTapGesture {
+                   let keyWindow = UIApplication.shared.connectedScenes
+                                      .filter({$0.activationState == .foregroundActive})
+                                      .map({$0 as? UIWindowScene})
+                                      .compactMap({$0})
+                                      .first?.windows
+                                      .filter({$0.isKeyWindow}).first
+                   keyWindow!.endEditing(true)
+            }
             
+            Text("Date / Time")
+            DatePicker(
+                selection: $selectedDate,
+                in: dateClosedRange,
+                displayedComponents: [.hourAndMinute, .date],
+                label: { Text("") }
+            )
         }
     }
-
+}
